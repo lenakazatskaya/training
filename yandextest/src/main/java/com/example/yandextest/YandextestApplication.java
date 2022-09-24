dfe55a0f17beecfa1323aa8cd6557acc04f10e61
Lena Kazatskaya <kazatskaya.lena@mail.ru>
2022-09-24 20:09:34 +0600

    yandextest

---
+++ yandextest/src/main/java/com/example/yandextest/YandextestApplication.java
@@ -0,0 +1,13 @@
+package com.example.yandextest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandextestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandextestApplication.class, args);
+	}
+
+}
